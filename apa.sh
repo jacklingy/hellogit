1e45f635bc126f57722a75cb26bac99111ce09b7
Jack Ren <49032893+jacklingy@users.noreply.github.com>
2019-06-11 21:36:58 +0800

    Update apa.sh
    sed

--- apa.sh
+++ apa.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
-sudo su
 apt update
 apt install wget -y
 apt install apache2 -y
 cd /var/www/html
-#rm index.html
+rm index.html
 wget http://jackling.cf/movies/Dark%20Phoenix%20_%20Official%20Trailer%20%5bHD%5d%20_%2020th%20Century%20FOX-1-q8C_c-nlM.mp4
